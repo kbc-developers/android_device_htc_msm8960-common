b5d67c877d7ff6194937800e650607b5b3f0601e
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-10-11 15:52:12 -0500

    msm8960: kangbang hiae-common's libril
    Change-Id: I123cf33a10bfbeafc83c436e9a11f118b088e657

--- libril/Android.mk
+++ libril/Android.mk
@@ -1,16 +1,9 @@
-# Copyright 2006 The Android Open Source Project
-
-ifeq ($(BOARD_PROVIDES_LIBRIL),true)
-ifeq ($(TARGET_BOARD_PLATFORM),msm8960)
-ifeq ($(BOARD_VENDOR),htc)
-
-
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES:= \
     ril.cpp \
-    ril_event.cpp \
+    ril_event.cpp\
     RilSocket.cpp \
     RilSapSocket.cpp \
 
@@ -20,33 +13,17 @@ LOCAL_SHARED_LIBRARIES := \
     libbinder \
     libcutils \
     libhardware_legacy \
-    librilutils
+    librilutils \
 
 LOCAL_STATIC_LIBRARIES := \
     libprotobuf-c-nano-enable_malloc \
 
-#LOCAL_CFLAGS := -DANDROID_MULTI_SIM -DDSDA_RILD1
-
-ifeq ($(SIM_COUNT), 2)
-    LOCAL_CFLAGS += -DANDROID_SIM_COUNT_2
-endif
-
-ifeq ($(BOARD_RIL_FIVE_SEARCH_RESPONSES),true)
-    LOCAL_CFLAGS += -DRIL_FIVE_SEARCH_RESPONSES
-endif
-
-LOCAL_C_INCLUDES += $(TARGET_OUT_HEADERS)/librilutils
 LOCAL_C_INCLUDES += external/nanopb-c
-
-LOCAL_CLANG := false
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../include
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/../include
 
 LOCAL_MODULE:= libril
-
-LOCAL_COPY_HEADERS_TO := libril
-LOCAL_COPY_HEADERS := ril_ex.h
+LOCAL_CLANG := true
+LOCAL_SANITIZE := integer
 
 include $(BUILD_SHARED_LIBRARY)
-
-endif # BOARD_VENDOR
-endif # TARGET_BOARD_PLATFORM
-endif # BOARD_PROVIDES_LIBRIL
